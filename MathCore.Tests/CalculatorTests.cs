4e726ca6eab68189f998e80059fd090feb56cf0a
quanptatb <quanptatb01758@gmail.com>
2025-12-09 13:58:57 +0700

    done kiem tra cong - chia cho khong

---
+++ MathCore.Tests/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OnThi1_KiemTraCong_Chia;
+using System;
+
+namespace MathCore.Tests
+{
+    [TestFixture] // đánh dấu lớp này là một bộ kiểm tra NUnit
+    public class CalculatorTests
+    {
+        private Calculator _cal;
+        [SetUp] // phương thức này sẽ được gọi trước mỗi phương thức kiểm tra
+        public void Setup()
+        {
+            _cal = new Calculator(); //khởi tạo đối tượng trước khi test
+        }
+
+        [Test] //đánh dấu đây là một bài test (testcase)
+        public void Cong_HaiSoDuong_TraVeKetQuaDung()
+        {
+            // Arrange (chuẩn bị dữ liệu)
+            int soA = 10;
+            int soB = 20;
+            int KetQuaMongDoi = 30;
+
+            // Act (thực hiện hành động cần kiểm tra)
+            int KetQuaThucTe = _cal.Cong(soA, soB);
+
+            // Assert (xác nhận kết quả)
+            Assert.That(KetQuaThucTe, Is.EqualTo(KetQuaMongDoi));
+        }
+        [Test]
+        public void Chia_ChoSoKhong_NemRaLoi()
+        {
+            Assert.That(() => _cal.Chia(10, 0), Throws.TypeOf<DivideByZeroException>()); //kiểm tra xem có ném ra ngoại lệ không
+        }
+    }
+}
---
+++ OnThi1_KiemTraCong_Chia/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace OnThi1_KiemTraCong_Chia
+{
+    public class Calculator
+    {
+        public int Cong(int a, int b)
+        {
+            return a + b;
+        }
+        public int Chia(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Khong the chia cho 0");
+            }
+            return a / b;
+        }
+    }
+}
